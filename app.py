41e7cb6effcaa3c10d119c45dae9c306a9576fb6
warut-vijit <warut.vijitbenjaronk@gmail.com>
2018-10-06 04:45:18 -0500

    split site into multiple pages

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import logging
 import os
 import sched
 import yaml
+import sys
 from datetime import datetime, timedelta
 from asset_manager import Spreadsheet, AssetFolder, authenticate_gdrive
 
@@ -41,14 +42,6 @@ def webhook():
 
 @app.route("/")
 def home():
-    return web_endpoint()
-
-@app.route("/<content>")
-def home_w_content(content):
-    return web_endpoint()
-
-def web_endpoint():
-
     # format news data
     if config['live_update']:
         news_data = news_datasheet.get_data()
@@ -63,35 +56,31 @@ def web_endpoint():
         """ % (news['date'], news['text']))
     news_formatted = "\n".join(news_arr)
 
-    # format staff data
+    # format sponsor data
     if config['live_update']:
-        staff_data = staff_datasheet.get_data()
+        sponsor_data = sponsor_datasheet.get_data()
     else:
-        staff_data = [] # for local development only
-    staff_arr = []
-    for index, staff in enumerate(staff_data):
-        start_row = ""
-        end_row = ""
-        if index % 4 == 0:
-            start_row = """<div class="row">"""
-        elif index % 4 == 3:
-            end_row = """</div>"""
+        sponsor_data = []
+    sponsor_arr = []
+    for sponsor in sponsor_data:
+        sponsor_arr.append("""
+            <section class="col-sm-3">
+                <a href = "%s" style="color:white;text-decoration: none">
+                    <img src="%s"/>
+                    <h2>%s</h2>
+                </a>
+            </section>
+        """ % (sponsor['link'], sponsor['image'], sponsor['name']))
+    sponsor_formatted = "\n".join(sponsor_arr)
 
-        border = "officer-bg" if staff["officer"] else "staff-bg"
+    return render_template(
+        "home.html",
+        news_formatted=news_formatted,
+        sponsor_formatted=sponsor_formatted,
+    )
 
-        staff_arr.append("""
-            %s
-            <div class="col-sm-3"> 
-                <div class="%s">
-                    <img src = "%s"/>
-                    <h4>%s</h4>
-                    <p>%s</p>
-                </div>  
-            </div>
-            %s
-        """ % (start_row, border, staff['profile'], staff['name'], staff['desc'], end_row))
-    staff_formatted = "\n".join(staff_arr)
-
+@app.route("/resources")
+def resources():
     # format lecture data
     if config['live_update']:
         lecture_data = lecture_datasheet.get_data()
@@ -114,29 +103,51 @@ def web_endpoint():
                lecture['video'], lecture['demo']))
     lecture_formatted = "\n".join(lecture_arr)
 
-    # format sponsor data
+    return render_template(
+        "resources.html",
+        lecture_formatted=lecture_formatted,
+    )
+
+@app.route("/staff")
+def staff():
+    # format staff data
     if config['live_update']:
-        sponsor_data = sponsor_datasheet.get_data()
+        staff_data = staff_datasheet.get_data()
     else:
-        sponsor_data = []
-    sponsor_arr = []
-    for sponsor in sponsor_data:
-        sponsor_arr.append("""
-            <section class="col-sm-3">
-                <a href = "%s" style="color:white;text-decoration: none">
-                    <img src="%s"/>
-                    <h2>%s</h2>
-                </a>
-            </section>
-        """ % (sponsor['link'], sponsor['image'], sponsor['name']))
-    sponsor_formatted = "\n".join(sponsor_arr)
+        staff_data = [] # for local development only
+    staff_arr = []
+    for index, staff in enumerate(staff_data):
+        start_row = ""
+        end_row = ""
+        if index % 4 == 0:
+            start_row = """<div class="row">"""
+        elif index % 4 == 3:
+            end_row = """</div>"""
+
+        border = "instructor-bg" if staff["officer"] else "staff-bg"
+
+        staff_arr.append("""
+            %s
+            <div class="col-sm-3"> 
+                <div class="%s">
+                    <img src = "%s"/>
+                    <h4>%s</h4>
+                    <p>%s</p>
+                </div>  
+            </div>
+            %s
+        """ % (start_row, border, staff['profile'], staff['name'], staff['desc'], end_row))
+    staff_formatted = "\n".join(staff_arr)
 
     return render_template(
-        "index.html",
-        news_formatted=news_formatted,
+        "staff.html",
         staff_formatted=staff_formatted,
-        lecture_formatted=lecture_formatted,
-        sponsor_formatted=sponsor_formatted,
+    )
+
+@app.route("/projects")
+def project():
+    return render_template(
+        "projects.html",
     )
 
 if __name__ == "__main__":
